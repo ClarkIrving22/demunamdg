6bab69deb11e69fa8555c445b15994211f3aae65
ClarkIrving22 <83539806+ClarkIrving22@users.noreply.github.com>
2023-03-13 16:58:38 -0500

    Add files via upload

---
+++ src/main/java/com/mdg/demuna/DemunaApplication.java
@@ -0,0 +1,13 @@
+package com.mdg.demuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdg/demuna/DemunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdg.demuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
